13c9e612dfde0e552314a505a38c4758ea684c82
DtD Productions <mike@dtdpro.com>
2013-01-08 22:16:59 -0500

    MUE Subscription Enforcer plugin

---
+++ plugins/pls_system_muesub/muesub.php
@@ -0,0 +1,39 @@
+<?php
+
+
+defined('_JEXEC') or die;
+
+class plgSystemMUESub extends JPlugin
+{
+	function onAfterRoute()
+	{
+		$app = JFactory::getApplication();
+
+		// No sub enforcement me for admin
+		if ($app->isAdmin()) {
+			return;
+		}
+
+		$user = JFactory::getUser();
+		if (JRequest::getVar('option') != "com_mue" && $user->id) {
+			// Load helper
+			require_once('components/com_mue'.DS.'helpers'.DS.'mue.php');
+			$config=MUEHelper::getConfig();
+			$numsubs=count(MUEHelper::getUserSubs());
+			if ($config->subscribe && $user->id) {
+				if ($numsubs) {
+					$sub=MUEHelper::getActiveSub();
+					if (!$sub) {
+						$app->redirect(JRoute::_('index.php?option=com_mue&view=user&layout=subs'));
+					} else {
+						if ((!$sub->sub_recurring || $sub->usrsub_rpstatus != "ActiveProfile") && $sub->daysLeft <= 10 && $this->params->get('warnexp', 0)) {
+							JError::raiseNotice('muesubexpiressoon','Subscription Expires in '.$sub->daysLeft. ' day(s)');
+						}
+					}
+				} else {
+					$app->redirect(JRoute::_('index.php?option=com_mue&view=user&layout=subs'));
+				}
+			}
+		}
+	}
+}
